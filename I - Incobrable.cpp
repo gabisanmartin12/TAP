8ed131ed92b22bb292a2f4c8b0665e0aed0315ef
gabisanmartin12 <gabisanmartin12@gmail.com>
2015-10-09 19:00:29 -0300

    Create I - Incobrable.cpp

---
+++ I - Incobrable.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int n;
+	while( cin >> n && n != -1 ){
+		int ac = 0, ir = 0;
+		for(int i=0;i<n;i++) { 
+			int pre;
+			cin >> pre;
+			ac += pre;
+			if( ac % 100 == 0 ){
+				ir++;
+				ac = 0;
+			}
+		}
+		cout << ir << endl;
+	}
+	return 0;
+}
